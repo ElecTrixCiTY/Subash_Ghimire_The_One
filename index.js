12fab27fe75ebc779dc3a273457ad7f9be66bf2b
ElecTrixCiTY <88834868+ElecTrixCiTY@users.noreply.github.com>
2022-06-06 23:40:07 +0545

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,36 @@
+
+
 var hamburger = document.querySelector(".hamburger");
 var navMenu = document.querySelector(".nav-menu");
 
-hamburger.addEventListener("click", ()=>{
+hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
 })
+
+
+var button1 = document.querySelector(".Dark");
+var footer1 = document.querySelector(".ft");
+
+
+
+button1.addEventListener("click", () => {
+    document.body.classList.remove("body");
+    document.body.classList.add("dark");
+    footer1.classList.add("footer2");
+    footer1.classList.remove("footer");
+   
+    
+
+})
+var button2 = document.querySelector(".Light");
+
+button2.addEventListener("click", ()=>{
+    document.body.classList.remove("dark");
+    document.body.classList.add("body");
+    footer1.classList.remove("footer2");
+    footer1.classList.add("footer");
+})
+
+
+
